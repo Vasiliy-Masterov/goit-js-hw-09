568759e82c1a9a1bc09b2b4717403e5df671412e
Vasiliy-Masterov <vasiliy.masterov@gmail.com>
2022-02-04 08:02:12 +0200

    main

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,8 +2,6 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-console.log("Hello");
-
 const buttonStartEl = document.querySelector('[data-start]');
 const buttonStopEl = document.querySelector('[data-stop]');
 const bodyEl = document.querySelector('body');
