8da635a1e3e4b485d7ebf9d5b14541b5450bd9b6
Vasiliy-Masterov <vasiliy.masterov@gmail.com>
2022-02-04 07:38:53 +0200

    gh-pages

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const buttonStartEl = document.querySelector('[data-start]');
+const buttonStopEl = document.querySelector('[data-stop]');
+const bodyEl = document.querySelector('body');
+let timerId = null;
+
+buttonStartEl.addEventListener('click', () => {
+    buttonStartEl.disabled = true;
+    timerId = setInterval(() => {
+    colorChange();
+    }, 1000);
+});
+ 
+buttonStopEl.addEventListener('click', event => {
+     buttonStartEl.disabled = false;
+     clearInterval(timerId);
+});
+
+function colorChange() {
+    const randomHexColor = getRandomHexColor();
+    bodyEl.style.backgroundColor = randomHexColor;   
+}     
 
